4473a9fd5b7fd06eb36c8cc3bd559fdc064f2588
TechCode <S19S93@yandex.ru>
2022-12-24 15:29:20 +0400

    change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import { Shop } from './components/Shop';
 import { FooterPage } from './components/Footer/Footer';
-// Routing configuration
-import { Routes, Route } from 'react-router-dom';
-import { About } from './assets/pages/About';
-import { BasketList } from './components/BasketList';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/cart" element={<BasketList />} />
-      </Routes>
+      <Shop />
       <FooterPage />
     </>
   );
